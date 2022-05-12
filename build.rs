1e38783e9c9ac49110b0b1f181a53ac6ccfaac3a
Kyllingene <fuzzymuffin343@gmail.com>
2022-05-12 13:50:17 -0700

    Made minor edits

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
+use std::env;
 use nasm_rs;
 
 fn main() {
-	nasm_rs::compile_library("libasm_main.a", &["src/asm_main.asm"]).unwrap();
+	if env::var("CARGO_CFG_TARGET_OS").unwrap() == "unix" {
+		nasm_rs::compile_library("libasm_main.a", &["src/asm_main.asm"]).unwrap();
+	} else {
+		nasm_rs::compile_library("asm_main.o", &["src/asm_main.asm"]).unwrap();
+	}
 }
